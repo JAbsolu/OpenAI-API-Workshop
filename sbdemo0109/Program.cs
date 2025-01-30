412d5da708b032f73cdbf73cd94b453a06e21a27
John Absolu <Johnson.absolu@hotmail.com>
2025-01-29 22:14:20 -0500

    Added all file

---
+++ sbdemo0109/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenAI_API;
+using dotenv.net;
+
+DotEnv.Load();
+
+string apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var api = new OpenAIAPI(apiKey);
+
+var request = new OpenAI_API.Images.ImageGenerationRequest
+{
+  Prompt = "A cute kitten in a basket"
+};
+
+// creata images
+var response = await api.ImageGenerations.CreateImageAsync(request);
+Console.WriteLine("Image URL: {0}", response.Data[0].Url);
